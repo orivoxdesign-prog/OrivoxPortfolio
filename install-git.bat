aeaea05fde240b10ad47706bc37041e599cbb168
orivoxdesign-prog <orivoxdesign@gmail.com>
2026-08-31 12:26:30 +0000

    fix : changement de modification sur les images du site

--- install-git.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Installation de Git...
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/download/v2.45.0.windows.1/Git-2.45.0-64-bit.exe' -OutFile '%temp%\GitInstaller.exe'; & '%temp%\GitInstaller.exe' /VERYSILENT /NORESTART"
-echo Installation terminée ! Redemarrez votre terminal PowerShell.
-pause
